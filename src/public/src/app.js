96554b1bd8beb50443954da72f61390fa15b99d6
j- <j@skeoh.com>
2016-02-13 22:35:47 +1100

    Observe drop events

--- src/public/src/app.js
+++ src/public/src/app.js
@@ -1,1 +1,17 @@
-// App logic goes here
+// import Buffer from 'buffer';
+// import magnetURI from 'magnet-uri';
+// import parseTorrent from 'parse-torrent';
+import Rx from 'rx';
+
+window.addEventListener('dragover', (e) => e.preventDefault());
+window.addEventListener('drop', (e) => e.preventDefault());
+
+const drops = Rx.Observable.fromEvent(window, 'drop');
+
+const dataTransfers = drops.map((e) => e.dataTransfer);
+
+const files = dataTransfers.selectMany((dt) => Rx.Observable.fromArray(dt.files));
+
+files.forEach(function (file) {
+	console.log('File', file);
+});
