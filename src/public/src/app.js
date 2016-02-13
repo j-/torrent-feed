dabe4e86b1db30726d7dd321b3a7a79d0f45a220
j- <j@skeoh.com>
2016-02-13 23:03:26 +1100

    Read files and parse torrents

--- src/public/src/app.js
+++ src/public/src/app.js
@@ -1,6 +1,6 @@
-// import Buffer from 'buffer';
-// import magnetURI from 'magnet-uri';
-// import parseTorrent from 'parse-torrent';
+import { Buffer } from 'buffer';
+import magnetURI from 'magnet-uri';
+import parseTorrent from 'parse-torrent';
 import Rx from 'rx';
 
 window.addEventListener('dragover', (e) => e.preventDefault());
@@ -12,6 +12,34 @@ const dataTransfers = drops.map((e) => e.dataTransfer);
 
 const files = dataTransfers.selectMany((dt) => Rx.Observable.fromArray(dt.files));
 
-files.forEach(function (file) {
-	console.log('File', file);
+const torrents = files
+	.selectMany((file) => {
+		const promise = new Promise((resolve, reject) => {
+			const reader = new FileReader();
+			reader.addEventListener('load', (e) => resolve(e.target.result));
+			reader.readAsBinaryString(file);
+		});
+		return Rx.Observable.fromPromise(promise);
+	})
+	.map((str) => new Buffer(str))
+	.map((buffer) => parseTorrent(buffer));
+
+torrents.forEach((torrent) => {
+	console.log('Torrent', torrent);
+	uploadTorrent(torrent);
 });
+
+function getUploadURL() {
+	return '/upload/7e710896-3899-49bb-bd97-c46833fdddcf';
+}
+
+function uploadTorrent (torrent) {
+	var uri = magnetURI.encode({
+		xt: torrent.xt,
+		tr: torrent.tr,
+	});
+	var req = new XMLHttpRequest();
+	req.open('POST', getUploadURL());
+	req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	req.send('data=' + encodeURIComponent(uri));
+}
